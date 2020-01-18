a04ce8cf314ce8f88ffe3dc95d2800e4c0213a19
sumeshi <j15322sn@gmail.com>
2020-01-19 02:01:47 +0900

    fix: separate class

--- src/hoge.rs
+++
@@ -1,7 +0,0 @@
-use std::collections::HashMap;
-
-pub fn hoge(filePath: &'static str) -> HashMap<&'static str, &'static str> {
-    let mut hmap = HashMap::new();
-    hmap.insert("hoge", filePath);
-    return hmap;
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,46 @@
 extern crate pyo3;
 
 use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
+//use pyo3::wrap_pyfunction;
 
-mod hoge;
+mod prefetch;
 
-#[pyfunction]
-fn get_dict(_py: Python<'_>, filePath: &'static str) -> PyObject {
-    let dict = PyObject::from(hoge::hoge(filePath).to_object(_py));
-    return dict;
+#[pyclass]
+struct Prefetch {
+    #[pyo3(get)]
+    name: String,
+    #[pyo3(get)]
+    exec_count: usize,
+    #[pyo3(get)]
+    last_exec_time: u64,
 }
+
+#[pymethods]
+impl Prefetch {
+    #[new]
+    fn new(paths: String) -> Self {
+        let p: prefetch::PyPrefetch = prefetch::PyPrefetch::new(paths);
+
+        let exec_count: usize = p.instance.execution_counter();
+        //let last_exec_time: i32 = prefetch::get_last_exec_time();
+        let last_exec_time: u64 = p.instance.last_execution_time();
+        let name: String = "test.pf".to_string();
+        //return Prefetch {paths, name, exec_count, last_exec_time};
+        Prefetch {name, exec_count, last_exec_time}
+    }
+}
+
+//#[pyfunction]
+//fn parse_json(_py: Python<'_>, file_path: &'static str) -> PyObject {
+//    let dict = PyObject::from(prefetch::hoge(file_path).to_object(_py));
+//    return dict;
+//}
 
 /// This module is a python module implemented in Rust.
 #[pymodule]
 fn prefetch(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(get_dict))?;
+    //m.add_wrapped(wrap_pyfunction!(parse_json))?;
+    m.add_class::<Prefetch>()?;
 
     Ok(())
 }
